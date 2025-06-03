3d51583118b7d0ed65d2cd4a32d1d93deacf607f
eastodev288 <infoeato288@gmail.com>
2025-06-03 22:18:15 +0600

    Vehicle_list

---
+++ student_app/lib/Constant/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color BgColor = Color.fromARGB(250, 251, 246, 233);
+  static Color ContainerBg = Colors.white54;
+
+  static const Color TextColor1 = Colors.black;
+  static const Color TextColor2 = Colors.white;
+}
